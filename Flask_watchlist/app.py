98f889cae29887ce745cb50f9b83723f789566d8
PETERMAOSX <petermaosx@gmail.com>
2021-06-03 22:40:39 +0800

    Add minimal home page

---
+++ Flask_watchlist/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import escape
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route("/home")
+def hello1():
+    return "主页"
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
